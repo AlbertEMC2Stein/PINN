0b02f99b147771b85d180553e3f4b235bfea11bc
TimProkosch <prokosch@rhrk.uni-kl.de>
2022-05-15 15:53:01 +0200

    Update makedocs.py

--- docs/makedocs.py
+++ docs/makedocs.py
@@ -2,12 +2,12 @@ import os
 from os.path import sep
 
 if __name__ == "__main__":
-    os.system("pdoc3 --html --config latex_math=True --output-dir . .." + sep + "src")
+    os.system("pdoc3 --html --config latex_math=True --output-dir . .." + sep + "PDESolver")
 
     assert os.getcwd().split(sep)[-1] == "docs"
 
-    for file in os.listdir("src"):
-        os.rename(os.getcwd() + sep + "src" + sep + file, os.getcwd() + sep + file)
+    for file in os.listdir("PDESolver"):
+        os.rename(os.getcwd() + sep + "PDESolver" + sep + file, os.getcwd() + sep + file)
 
-    os.rmdir("src")
+    os.rmdir("PDESolver")
 
