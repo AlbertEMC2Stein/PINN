87bbf4b29e256a100712a1904e4804223ee7681a
TimProkosch <prokosch@rhrk.uni-kl.de>
2022-05-16 13:06:54 +0200

    Did some further testing

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ from PDESolver.BoundaryValueProblem import *
 
 
 if __name__ == "__main__":
-    bvp = ControlledHeatEquation1D
+    bvp = KortewegDeVriesEquation
 
     # Number of iterations
     N = 5000
 
     # Initialize model, learning rate scheduler and choose optimizer
-    solver = Solver(bvp, num_inputs=2, num_outputs=2)
-    lr = tf.keras.optimizers.schedules.PolynomialDecay(0.1, N, 1e-5, 2)
+    solver = Solver(bvp, num_inputs=2, num_outputs=1)
+    lr = tf.keras.optimizers.schedules.PolynomialDecay(0.01, N, 1e-3, 2)
     optim = tf.keras.optimizers.Adam(learning_rate=lr)
 
     #tf.Variable(1., trainable=True, name="lambda")
