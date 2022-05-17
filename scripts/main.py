ec2e4eedda53732828a41f5b0a753f14b03f63fd
TimProkosch <prokosch@rhrk.uni-kl.de>
2022-05-17 14:30:01 +0200

    Reformatted code

--- scripts/main.py
+++ scripts/main.py
@@ -1,21 +1,20 @@
 from PDESolver import *
+import tensorflow as tf
 
+bvp = KortewegDeVriesEquation
 
-if __name__ == "__main__":
-    bvp = KortewegDeVriesEquation
+# Number of iterations
+N = 5000
 
-    # Number of iterations
-    N = 5000
+# Initialize model, learning rate scheduler and choose optimizer
+solver = Solver(bvp, num_inputs=2, num_outputs=1)
+lr = tf.keras.optimizers.schedules.PolynomialDecay(0.01, N, 1e-3, 2)
+optim = tf.keras.optimizers.Adam(learning_rate=lr)
 
-    # Initialize model, learning rate scheduler and choose optimizer
-    solver = Solver(bvp, num_inputs=2, num_outputs=1)
-    lr = tf.keras.optimizers.schedules.PolynomialDecay(0.01, N, 1e-3, 2)
-    optim = tf.keras.optimizers.Adam(learning_rate=lr)
+# tf.Variable(1., trainable=True, name="lambda")
+solver.train(optim, lr, N)
 
-    #tf.Variable(1., trainable=True, name="lambda")
-    solver.train(optim, lr, N)
-
-    #plot_phaseplot()
-    #plot_2D()
-    #plot_2Dvectorfield()
-    plot_1D(solver)
+# plot_phaseplot()
+# plot_2D()
+# plot_2Dvectorfield()
+plot_1D(solver)
