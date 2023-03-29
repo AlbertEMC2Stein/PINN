49777ad66246e78ca7496f9f70359309de891333
Tim Prokosch <tim.prokosch@t-online.de>
2023-03-29 19:47:49 +0200

    Test

--- scripts/BurgersEquation.py
+++ scripts/BurgersEquation.py
@@ -1,11 +1,11 @@
 from PDESolver import *
 
 # Number of iterations
-N = 5000
+N = 20000
 
 # Initialize solver, learning rate scheduler and choose optimizer
-solver = Solver(BurgersEquation(viscosity=0.0001), num_hidden_layers=4, num_neurons_per_layer=50)
-lr = tf.keras.optimizers.schedules.ExponentialDecay(0.001, decay_steps=500, decay_rate=0.9)
+solver = Solver(BurgersEquation(viscosity=0.0), num_hidden_layers=4, num_neurons_per_layer=50)
+lr = tf.keras.optimizers.schedules.ExponentialDecay(0.01, decay_steps=250, decay_rate=0.9)
 optim = tf.keras.optimizers.Adam(learning_rate=lr)
 
 # Train model and plot results
