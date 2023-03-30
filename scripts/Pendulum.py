3e30743b1d4b0f47b3a0314412eb32967711a3ab
Tim Prokosch <tim.prokosch@t-online.de>
2023-03-30 08:09:58 +0200

    Tested pendulum with new process structure

--- scripts/Pendulum.py
+++ scripts/Pendulum.py
@@ -1,11 +1,11 @@
 from PDESolver import *
 
 # Number of iterations
-N = 40000
+N = 5000
 
 # Initialize solver, learning rate scheduler and choose optimizer
-solver = Solver(Pendulum(), num_inputs=1, num_outputs=3, num_hidden_layers=4, num_neurons_per_layer=50)
-lr = tf.keras.optimizers.schedules.ExponentialDecay(0.01, decay_steps=500, decay_rate=0.9)
+solver = Solver(Pendulum(), num_hidden_layers=4, num_neurons_per_layer=50)
+lr = tf.keras.optimizers.schedules.ExponentialDecay(0.001, decay_steps=500, decay_rate=0.9)
 optim = tf.keras.optimizers.Adam(learning_rate=lr)
 
 # Train model and plot results
