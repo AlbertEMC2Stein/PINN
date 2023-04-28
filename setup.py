df69f20f833ea548d23ddb75baeba696056a291b
Tim Prokosch <tim.prokosch@t-online.de>
2023-04-28 08:19:04 +0200

    Updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='PDESolver',
-    version='1.0.0',
+    version='1.1.0',
     python_requires='>=3.7',
     packages=find_packages(include=['PDESolver']),
     install_requires=[
