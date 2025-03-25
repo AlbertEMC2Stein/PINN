921ba13eeaf3716b060d2132a4ccaab9f95fe9d1
Tim Prokosch <prokosch@rhrk.uni-kl.de>
2025-03-25 13:22:32 +0100

    Updated to 3.11

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='PDESolver',
-    version='1.1.0',
-    python_requires='>=3.7',
+    version='1.2.0',
+    python_requires='>=3.11',
     packages=find_packages(include=['PDESolver']),
     install_requires=[
-        'tensorflow >= 2.8.0',
-        'numpy >= 1.21.3',
-        'matplotlib >= 3.4.3'
+        'tensorflow_cpu >= 2.19.0',
+        'numpy >= 2.1.0',
+        'matplotlib >= 3.10.0',
+        'tqdm >= 4.67.0',
     ]
 )
